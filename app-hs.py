ff0d2807f3a22f336b23620cacd82ab78e54aa29
AleeX99tmb <131254172+AleeX99tmb@users.noreply.github.com>
2024-05-24 10:19:52 +0000

    Add files via upload

--- app-hs.py
+++ app-hs.py
@@ -17,6 +17,14 @@ def buscar_handshakes(salida_tshark):
 
 
 if __name__ == "__main__":
+    subprocess.run(["airmon-ng", "check", "kill"])
+    subprocess.run(["airmon-ng", "start", "wlx00e04c093e49"])
+   
+    proceso_airodump = subprocess.Popen(["sudo", "airodump-ng", "-c", "11", "-w", "allthedata", "--bssid", "E8:9C:25:B0:C4:40", "wlx00e04c093e49"])
+    time.sleep(120)
+    proceso_airodump.terminate()
+    proceso_airodump.wait()
+
     nombre_archivo = "allthedata-01.cap"
     salida_tshark = ejecutar_tshark(nombre_archivo)
     handshakes = buscar_handshakes(salida_tshark)
