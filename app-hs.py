4f6dae4f89175edbc14d60a7c2f55c1cba9f2511
AleeX99tmb <131254172+AleeX99tmb@users.noreply.github.com>
2024-05-28 10:59:06 +0200

    Add files via upload

--- app-hs.py
+++ app-hs.py
@@ -1,7 +1,16 @@
 import subprocess
+import time
+
+def airodump():
+    subprocess.run(["sudo","rm", "/home/alex/allthedata-01.cap", "/home/alex/allthedata-01.csv", "/home/alex/allthedata-01.kismet.csv", "/home/alex/allthedata-01.kismet.netxml","/home/alex/allthedata-01.log.csv"])
+    subprocess.run(["airmon-ng", "check", "kill"])
+    subprocess.run(["airmon-ng", "start", "wlx00e04c093e49"])
+    proceso_airodump = subprocess.Popen(["sudo", "airodump-ng", "-c", "11", "-w", "allthedata", "--bssid", "E8:9C:25:B0:C4:40", "wlx00e04c093e49"])
+    time.sleep(10)
+    proceso_airodump.kill()
+    proceso_airodump.wait()
 
 def ejecutar_tshark(nombre_archivo):
-    # Ejecutar tshark para leer el archivo de captura
     comando = ["tshark", "-r", nombre_archivo]
     proceso = subprocess.Popen(comando, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     salida, _ = proceso.communicate()
@@ -13,18 +22,11 @@ def buscar_handshakes(salida_tshark):
     for line in lines:
         if "EAPOL" in line and "Key (Message" in line:
             campos = line.split()
-            print(f"Mensaje encontrado: {line}")  # Imprimir el mensaje para depuración
-
+            print(f"Mensaje encontrado: {line}")  
+    return handshakes
 
 if __name__ == "__main__":
-    subprocess.run(["airmon-ng", "check", "kill"])
-    subprocess.run(["airmon-ng", "start", "wlx00e04c093e49"])
-   
-    proceso_airodump = subprocess.Popen(["sudo", "airodump-ng", "-c", "11", "-w", "allthedata", "--bssid", "E8:9C:25:B0:C4:40", "wlx00e04c093e49"])
-    time.sleep(120)
-    proceso_airodump.terminate()
-    proceso_airodump.wait()
-
+    airodump()
     nombre_archivo = "allthedata-01.cap"
     salida_tshark = ejecutar_tshark(nombre_archivo)
     handshakes = buscar_handshakes(salida_tshark)
